bc44a291407174e1c1718c8a188db5624feba0c3
Pedro Villanueva <pedvide@gmail.com>
2020-11-09 22:25:59 +0100

    fix errors

--- influxdb/setup_influxdb.sh
+++ influxdb/setup_influxdb.sh
@@ -2,9 +2,11 @@
 
 sudo useradd -rs /bin/false influxdb
 sudo mkdir -p /var/lib/influxdb
-sudo chown influxdb:influxdb /var/lib/influxdb
 
 docker run --rm -e INFLUXDB_HTTP_AUTH_ENABLED=true \
 	-e INFLUXDB_ADMIN_USER=influx_admin -e INFLUXDB_ADMIN_PASSWORD=influx_admin123 \
 	-v /var/lib/influxdb:/var/lib/influxdb \
+    --user "$(id -u influxdb)":"$(id -g influxdb)" \
 	influxdb /init-influxdb.sh
+
+sudo chown influxdb:influxdb /var/lib/influxdb/**/*
