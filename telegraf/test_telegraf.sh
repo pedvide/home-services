77c062b7abcb8b6ac840c1cbec5e34ff7f16e501
Pedro Villanueva <pedvide@gmail.com>
2020-11-09 23:23:01 +0100

    fix old policy

--- telegraf/test_telegraf.sh
+++ telegraf/test_telegraf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 curl -sG -u influx_admin:influx_admin123 http://localhost:8086/query \
-	--data-urlencode "q=SELECT * FROM telegraf.autogen.cpu LIMIT 1" | jq
+	--data-urlencode "q=SELECT * FROM telegraf.monthly.cpu LIMIT 1" | jq
